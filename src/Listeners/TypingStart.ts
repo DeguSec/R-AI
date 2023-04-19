169d130c02c20683045d2f17558f5a425b0768f6
Rijndael <rijndael@baldy.ga>
2023-04-19 19:32:42 +0100

    Added extra redundancy

--- src/Listeners/TypingStart.ts
+++ src/Listeners/TypingStart.ts
@@ -2,11 +2,13 @@ import { Typing } from "discord.js";
 import { GetAI } from "../Functions/GetAI";
 import { CheckAllowedSource } from "../Functions/CheckAllowedSource";
 import { CommonComponents } from "../CommonComponents";
-import { AIController } from "../AI/AIController";
 
 export const TypingStartFunction = async (typing: Typing, cc: CommonComponents) => {
     if (await CheckAllowedSource(cc, typing.channel.id, typing.guild?.id)) {
-        const ai = GetAI(cc, typing.channel) as AIController; // check allowed source only returns true if inside AI pool
+        const ai = GetAI(cc, typing.channel); // check allowed source only returns true if inside AI pool
+        if(!ai)
+            return;
+
         ai.typing(typing);
     }
 }
