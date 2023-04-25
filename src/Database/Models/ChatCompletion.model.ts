7862768322ef465639e44215827ca8e9a5149f5c
Rijndael <rijndael@baldy.ga>
2023-04-25 03:13:43 +0100

    Fixed DB model

--- src/Database/Models/ChatCompletion.model.ts
+++ src/Database/Models/ChatCompletion.model.ts
@@ -1,8 +1,20 @@
-import { Schema } from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import { messageContentSchema } from "./MessageContent.model";
+import { ChatCompletionRequestMessage } from "openai";
 
 export const chatCompletionSchema: Schema = new Schema({
     status: {type: String, required: true},
     content: {type: messageContentSchema, required: true}
 
 }, { timestamps: { createdAt: 'Created', updatedAt: 'LastUpdated' } });
+
+export const ChatCompletionModel = mongoose.model('ChatCompletion', chatCompletionSchema);
+
+export interface IChatCompletionModel {
+    _id?: string | null;
+    status: "Completed" | "Pending" | "Failed";
+    content: ChatCompletionRequestMessage;
+}
+
+export interface IChannelEntity extends Omit<IChatCompletionModel, '_id'> {
+}
