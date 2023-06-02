813ee3b2acbc1d89ee7c1db6ef46b466408eed0c
Lukasz Baldyga <rijndael@baldy.ga>
2023-06-03 00:59:29 +0100

    now scheduling everything

--- src/AI/Voice/AIVoiceScheduler.ts
+++ src/AI/Voice/AIVoiceScheduler.ts
@@ -4,12 +4,19 @@ import { CommonComponents } from "../../CommonComponents";
 import { AIVoiceUser } from "./AIVoiceUser";
 import { SyncPersonality } from "../Base/AISyncPersonality";
 import { DEFAULT_PERSONALITY_STRING } from "../../Defaults";
+import { stripBad } from "../../Functions/UserFunctions";
+
+const scheduling = 3000;
 
 /**
  * Per channel operations
  */
 export class VoiceScheduler {
+    // all the users in the voice
     users: Map<string, AIVoiceUser> = new Map();
+
+    // all the calls and stuff
+    userDataScheduling: Map<string, NodeJS.Timeout> = new Map();
 
     // AI Stuff
     personality: SyncPersonality;
@@ -37,7 +44,24 @@ export class VoiceScheduler {
 
     addData(user: GuildMember, data: Buffer) {
         this.getUser(user).addData(data);
+
+        // schedule an API call to the thing and remove previous one if it exists
+        if(this.userDataScheduling.has(user.user.id))
+            clearTimeout(this.userDataScheduling.get(user.user.id));
+
+        this.userDataScheduling.set(user.user.id, setTimeout(async () => {
+            this.userDataScheduling.delete(user.user.id);
+            if(this.userDataScheduling.size == 0)
+                await this.react();
+
+        }, scheduling));
     }
 
-    
+    addUserMessage(time: Date, user: string, message: string) {
+        this.personality.addUserMessage(message, stripBad(user));
+    }
+
+    async react() {
+        console.log("Reacting...");
+    }
 }
