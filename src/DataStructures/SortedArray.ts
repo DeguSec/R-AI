0fdea25447c8c1ed520afa7ab4a11f5afd4902dc
Lukasz Baldyga <rijndael@baldy.ga>
2023-06-03 22:38:10 +0100

    Implementing a sorted array

--- src/DataStructures/SortedArray.ts
+++ src/DataStructures/SortedArray.ts
@@ -12,11 +12,56 @@ export class SortedArray<K, T> {
     items: Array<SortedItem<K, T>> = [];
 
     insert(key: K, item: T) {
-        // find the right spot to insert
+        // get potential item
+        const index = this.findIndex(key);
+
+        // goes off the far end of the array
+        if(index == this.items.length) {
+            this.items.push(new SortedItem<K, T>(key, item));
+        }
     }
 
     get(key: K) {
-        // 
+        // get potential item
+        const index = this.findIndex(key);
+
+        // goes off the far end of the array
+        if(index == this.items.length)
+            return null;
+        
+        // possibly item in array
+        const item = this.items[index];
+
+        if(item.key == key)
+            return item.value;
+
+        return null;
+    }
+
+    private findIndex(key: K, start?: number, end?: number): number {
+        // init vars
+        start = start ?? 0;
+        end = end ?? this.items.length;
+
+        // return the index
+        if(start == end)
+            return start;
+
+        // find the midpoint
+        const checkIndex = ((start + end) / 2) % 1; 
+        console.log(checkIndex);
+
+        // find the key at index 
+        const checkItem = this.items[checkIndex].key;
+
+        if(checkItem == key) 
+            return checkIndex;
+
+        else if(checkItem > key)
+            return this.findIndex(key, checkIndex + 1, end);
+
+        else // checkItem < key
+            return this.findIndex(key, start, checkIndex - 1);
     }
 
     clear() {
