032b07165e4ca9e7cd6d5f010f56bf0b6d4e039b
Lukasz Baldyga <rijndael@baldy.ga>
2023-06-18 12:11:50 +0100

    fixed a found bug through tests
    | was an allowed character.

--- src/Functions/UserFunctions.ts
+++ src/Functions/UserFunctions.ts
@@ -1,4 +1,4 @@
 import { User } from "discord.js";
 
-export const stripBad = (text: string) => text.replace(/[^A-Z|a-z|0-9]/g, "");
+export const stripBad = (text: string) => text.replace(/[^A-Za-z0-9]/g, "");
 export const convertUserForBot = (user: User) => `${stripBad(user.username)}${user.discriminator}`;
